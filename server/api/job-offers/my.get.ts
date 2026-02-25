591292e6dd585adf7b16616f45b0767464fb9a69
Rami <rami.aouinti@gmail.com>
2026-02-25 15:35:59 +0100

    Add job-offers my and available GET proxy endpoints

---
+++ server/api/job-offers/my.get.ts
@@ -0,0 +1,10 @@
+import { proxyAuthApiRequest } from '../../utils/auth-api-proxy'
+import { buildQuerySuffix } from '../../utils/query-string'
+import { requireAuthenticatedRequest } from '../../utils/require-auth'
+
+export default defineEventHandler(async (event) => {
+  requireAuthenticatedRequest(event)
+  const suffix = buildQuerySuffix(event)
+
+  return await proxyAuthApiRequest(event, `/api/v1/job-offers/my${suffix}`, 'GET')
+})
