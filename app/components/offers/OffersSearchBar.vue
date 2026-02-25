1717d56f6b6d43683fcce60a895d92a01b3d3b2e
Rami <rami.aouinti@gmail.com>
2026-02-25 20:59:45 +0100

    feat(offers): move search controls to app-bar and horizontal filters

--- app/components/offers/OffersSearchBar.vue
+++ app/components/offers/OffersSearchBar.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   title?: string
   subtitle?: string
+  appBarTeleport?: boolean
 }>()
 
 const query = defineModel<string>('query', { default: '' })
@@ -13,12 +14,50 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <section class="offers-search-bar">
+  <section
+    class="offers-search-bar"
+    :class="{ 'offers-search-bar--app-bar': props.appBarTeleport }"
+  >
     <div v-if="title || subtitle" class="offers-search-bar__header">
       <h1>{{ title }}</h1>
       <p>{{ subtitle }}</p>
     </div>
-    <div class="offers-search-bar__form">
+    <div
+      v-if="props.appBarTeleport"
+      class="offers-search-bar__form offers-search-bar__form--teleport"
+    >
+      <client-only>
+        <teleport to="#app-bar">
+          <div class="offers-search-bar__teleport-content">
+            <v-text-field
+              v-model="query"
+              label="Jobtitel oder Stichwort"
+              hide-details
+              variant="outlined"
+              density="comfortable"
+              prepend-inner-icon="mdi-magnify"
+            />
+            <v-text-field
+              v-model="location"
+              label="Ort oder Remote"
+              hide-details
+              variant="outlined"
+              density="comfortable"
+              prepend-inner-icon="mdi-map-marker-outline"
+            />
+            <v-btn
+              class="offers-search-bar__cta"
+              color="primary"
+              size="large"
+              @click="emit('search')"
+            >
+              Jobs finden
+            </v-btn>
+          </div>
+        </teleport>
+      </client-only>
+    </div>
+    <div v-else class="offers-search-bar__form">
       <v-text-field
         v-model="query"
         label="Jobtitel oder Stichwort"
@@ -35,7 +74,12 @@ const emit = defineEmits<{
         density="comfortable"
         prepend-inner-icon="mdi-map-marker-outline"
       />
-      <v-btn class="offers-search-bar__cta" color="primary" size="large" @click="emit('search')">
+      <v-btn
+        class="offers-search-bar__cta"
+        color="primary"
+        size="large"
+        @click="emit('search')"
+      >
         Jobs finden
       </v-btn>
     </div>
