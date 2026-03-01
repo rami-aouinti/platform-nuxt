6ba286c27d994a232968dcff23ca9b61aeee40f9
Rami Aouinti <rami.aouinti@gmail.com>
2026-03-01 19:08:20 +0100

    fix tem

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,32 @@
+<script setup lang="ts">
+import VercelAnalyticsPlaceholder
+  from "~/components/layout/analytics/VercelAnalyticsPlaceholder";
+import SpeedInsightsPlaceholder from "~/components/layout/analytics/SpeedInsightsPlaceholder";
+const LazyAnalytics = defineAsyncComponent({
+  loader: async () => {
+    if (import.meta.server) {
+      return VercelAnalyticsPlaceholder;
+    }
+
+    const module = await import("@vercel/analytics/vue");
+    return module.Analytics;
+  },
+  suspensible: false,
+});
+
+const LazySpeedInsights = defineAsyncComponent({
+  loader: async () => {
+    if (import.meta.server) {
+      return SpeedInsightsPlaceholder;
+    }
+
+    const module = await import("@vercel/speed-insights/nuxt");
+    return module.SpeedInsights;
+  },
+  suspensible: false,
+});
+
+</script>
 <template>
   <div class="default-layout">
     <AppBar
@@ -10,6 +39,16 @@
       <slot />
     </v-main>
     <AppFooter />
+    <ClientOnly>
+      <component
+        :is="LazyAnalytics"
+        v-if="shouldRenderAnalytics"
+      />
+      <component
+        :is="LazySpeedInsights"
+        v-if="shouldRenderSpeedInsights"
+      />
+    </ClientOnly>
   </div>
 </template>
 
