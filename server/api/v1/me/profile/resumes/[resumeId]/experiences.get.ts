7d18c9bc9475a15cf994655d05861224e388c221
Rami <rami.aouinti@gmail.com>
2026-03-02 15:50:19 +0100

    feat(api): add dedicated v1 profile resume handlers and governance inventory

---
+++ server/api/v1/me/profile/resumes/[resumeId]/experiences.get.ts
@@ -0,0 +1,7 @@
+import { createProxyEntityHandler } from '../../../../../../utils/proxy-handler-factory'
+
+export default createProxyEntityHandler({
+  paramName: 'resumeId',
+  method: 'GET',
+  upstreamPathBuilder: resumeId => `/api/v1/me/profile/resumes/${resumeId}/experiences`,
+})
