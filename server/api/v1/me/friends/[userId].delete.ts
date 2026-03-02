3e142aafe8bb3ed17f55fe48c60dd04584a4cc93
Rami <rami.aouinti@gmail.com>
2026-03-02 22:00:45 +0100

    feat(profile): add friends tab and v1 friends proxies

---
+++ server/api/v1/me/friends/[userId].delete.ts
@@ -0,0 +1,11 @@
+import { createProxyEntityHandler } from '../../../../utils/proxy-handler-factory'
+
+export default createProxyEntityHandler({
+  paramName: 'userId',
+  method: 'DELETE',
+  missingParamError: {
+    statusMessage: 'Invalid user parameter.',
+    message: 'User identifier is required.',
+  },
+  upstreamPathBuilder: userId => `/api/v1/me/friends/${encodeURIComponent(userId)}`,
+})
